24ab437de26c1d093e23113254c3770389a2d95d
Tamas Matyas <matyas_tommy@hotmail.com>
2024-09-04 20:00:12 +0200

    finished data id endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,52 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+
 const app = express();
 const port = 3000;
 
-app.get("/frontend", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/../frontend/index.html"));
 });
 
 app.use("/public", express.static(path.join(__dirname, "/../frontend/public")));
 
 app.get("/data", (req, res) => {
-  res.json({ name: "kismacska" });
+  fs.readFile(`${__dirname}/data.json`, (err, data) => {
+    if (err) {
+      console.log("error at reading the file", err);
+      res.json("error at reading the file");
+    } else {
+      const jsonData = JSON.parse(data);
+      const result = jsonData.map((obj) => obj.id);
+      res.json(result);
+    }
+  });
+});
+
+app.get("/data/:id", (req, res) => {
+  console.log(req.params);
+  const searchId = Number(req.params.id);
+
+  if (isNaN(searchId)) {
+    res.status(400).json(`id must be number`);
+  } else {
+    fs.readFile(`${__dirname}/data.json`, (err, data) => {
+      if (err) {
+        console.log("error at reading the file", err);
+        res.status(500).json("error at reading the file");
+      } else {
+        const jsonData = JSON.parse(data);
+        const result = jsonData.find((obj) => obj.id === searchId);
+
+        if (result === undefined) {
+          res.status(404).json(`no user found with id ${searchId}`);
+        } else {
+          res.status(200).json(result);
+        }
+      }
+    });
+  }
 });
 
 app.listen(port, () => {
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -3,3 +3,15 @@ console.log("loaded");
 fetch("/data")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+fetch("/data/kismacska")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+fetch("/data/120")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+fetch("/data/12")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
